18a5f51fa9e85c9891c6c1794e475d9f30be0a3f
Mitu kr Rabha <mitukr2@gmail.com>
2024-06-09 18:36:51 +0530

    Add files via upload

---
+++ diagonal.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class diagonal {
+    public static int diagonalSum(int matrix[][]){
+        int sum = 0;
+
+        // for(int i = 0; i < matrix.length; i++){
+        //     for(int j = 0; j < matrix[0].length; j++){
+        //         if(i == j || i + j == matrix.length - 1){
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+
+        for(int i=0; i<matrix.length; i++){
+            //pd
+            sum += matrix[i][i];
+            //sd
+            if(1 != matrix.length-1-i)
+                sum +=matrix[i][matrix.length-i-1];
+            
+        }
+        return sum;
+    }
+
+    public static void main(String args[]){
+        int matrix[][]={{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+
+        int result = diagonalSum(matrix);
+        System.out.println("Sum of diagonals: " + result);
+    }
+}
